a87520b3c75aee942bfe02a35a30b6a5d5b6b927
Richard Widén <richardwiden@gmail.com>
2016-09-25 15:42:48 +0200

    css to make room for comments

--- web/scripts/experience.js
+++ web/scripts/experience.js
@@ -9,31 +9,51 @@ whoami.Experience = class {
    */
   constructor() {
     this.signifierCssClasses = 'whoami-personal whoami-professional whoami-school';
+    this.hideCommentsCss = 'hide-comments';
+    this.showCommentsCss = 'show-comments';
     this.experienceElement = $(whoami.Experience.createExperienceHtml());
     this.titleElement = $('h4', this.experienceElement);
     this.descElement = $('.mdl-card__supporting-text', this.experienceElement);
     this.signifierElements = $('.mdl-card__title, .mdl-card__actions', this.experienceElement);
     this.signifierElements.push(this.experienceElement);
-    $('button', this.experienceElement).click(()=> {
-      this.readAndCommentClick();
+    this.moreButton = $('button', this.experienceElement);
+    this.moreButton.click(()=> {
+      this.moreToggle();
     });
     this.showMore = false;
+    this.commentsElement = $('.comments', this.experienceElement);
+
+
     $(document).ready(()=> {
     });
   }
 
 
-  readAndCommentClick() {
+  moreToggle() {
     this.showMore = !this.showMore;
 
     const normalClasses = 'experience-card mdl-card mdl-shadow--2dp mdl-cell mdl-cell--12-phone mdl-cel--6-tablet mdl-cell--4-col';
     const activeClasses = 'experience-card mdl-card mdl-shadow--2dp mdl-cell mdl-cell--12-phone mdl-cel--12-tablet mdl-cell--12-col';
 
     this.experienceElement.toggleClass(normalClasses).toggleClass(activeClasses);
+    this.setImage(this.showMore, this.desc, this.image);
 
-    if (this.showMore) {
-      this.experienceElement
+    this.commentsElement.toggleClass(this.hideCommentsCss).toggleClass(this.showCommentsCss);
+    if (this.showMore) { // is showing more
+      this.moreButton.html('Less <i class="material-icons">more</i>');
     }
+    else {
+      this.moreButton.html('More & Comment <i class="material-icons">more</i> ');
+    }
+
+    if(this.showMore)
+      this.loadAndListenForComments();
+    else
+      this.stopListeningForComments();
+  }
+
+  loadAndListenForComments(){
+    firebase.database
   }
 
   /**
@@ -63,10 +83,16 @@ whoami.Experience = class {
       jqElem.addClass('whoami-' + type).addClass(cssClasses);
     });
 
-    const expImage = $('.mdl-card__supporting-text', expElement);
-    if ((!desc || desc == '') && image)
-      expImage.attr("style", "background-image: url('/images/" + image + "'); background-size: cover; background-position: center center;");
+    this.setImage(this.showMore, desc, image);
     return expElement;
+  }
+
+  setImage(showMore, desc, image) {
+    let height = '';
+    if (showMore)
+      height = 'height:50vh;max-height:400px;';
+    if ((!desc || desc == '') && image)
+      this.descElement.attr("style", "background-image: url('/images/" + image + "'); background-size: cover; background-position: center center;flex-grow:1;padding:0;width:100%;" + height);
   }
 
   /**
@@ -75,17 +101,18 @@ whoami.Experience = class {
    */
   static createExperienceHtml() {
     return `
-    
       <div class="experience-card mdl-card mdl-shadow--2dp mdl-cell mdl-cell--12-phone mdl-cel--6-tablet mdl-cell--4-col">            
-          <div class="mdl-card__title" >              
-              <h4></h4>              
-          </div>
-          <div class="mdl-card__supporting-text  mdl-card--expand"></div>          
-          <div class="mdl-card__actions mdl-card--border">
-              <button class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
-                  Read more & Comment <i class="material-icons">more</i>                                                        
-               </button>            
-      </div>
+            <div class="mdl-card__title" >              
+                <h4></h4>              
+            </div>
+            <div class="mdl-card__supporting-text  mdl-card--expand"></div>
+            <div class="comments mdl-card hide-comments "></div>
+            <div class="mdl-card__actions mdl-card--border">          
+              <button class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" value="">
+                    More & Comment <i class="material-icons">more</i>                                                        
+               </button>    
+            </div>
+        </div>
     
     `;
   }
--- web/scripts/firebase.js
+++ web/scripts/firebase.js
@@ -9,8 +9,16 @@ whoami.Firebase = class {
    */
   constructor() {
     this.database = firebase.database();
+    this.refs ={};
     $(document).ready(()=> {
 
+    });
+  }
+
+  getComments(experienceId, cb) {
+
+    let ref = this.database.ref('/comments').orderByChild('experienceId').equalTo(experienceId).on('child_added', (data) => {
+      return cb(null, data.val(),data.key)
     });
   }
 
@@ -20,9 +28,9 @@ whoami.Firebase = class {
   }
 
 
-  addExperience(title, type, desc, image, year, cb){
+  addExperience(title, type, desc, image, year, cb) {
     let ref = this.database.ref('/experiences');
-    return ref.push({title:title,type:type,desc:desc,image:image,year:year});
+    return ref.push({title: title, type: type, desc: desc, image: image, year: year});
   }
 
   getExperiences(cb) {
